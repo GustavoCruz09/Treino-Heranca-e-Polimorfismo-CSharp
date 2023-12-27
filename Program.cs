ed8c37ffdadd3ca8165e0e7c8c2f90fb5a50a2d4
Gustavo da Cruz Santos <dacruz.gustavo09@gmail.com>
2023-12-26 22:32:31 -0300

    Conclusão do Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste para o Iphone");
+Iphone iphone = new Iphone("40028922", "Iphone 15 pro max", "123", 5000);
+iphone.InstalarAplicativo("Apple Store");
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+Console.WriteLine("\nTeste para o Nokia");
+Nokia nokia = new Nokia("22982004", "nokia tijolão", "123", 6500);
+nokia.InstalarAplicativo("Play Store");
+nokia.Ligar();
+nokia.ReceberLigacao();
